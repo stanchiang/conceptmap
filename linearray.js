e6eb3b798a54f88f874b2c29a2f40bdc7021aa21
stanchiang <stanchiang23@gmail.com>
2014-07-13 08:31:31 -0400

    complete graph json structure

--- linearray.js
+++ linearray.js
@@ -1,31 +1,26 @@
-edges = {
-	"line0": 
+//i want to go from w=>linearray[1][1]
+data = 
+{
+	"graph":{
+		"author":["stan", "bob", "joe"],
+		"title":"example graph"
+	},
+	"nodes":
 		[
-			{"node0":[1,2],"text":"aaa"},
-			{"node1":[3,4],"text":"bbb"}
+			{"id":0, "coord":[1,2],"text":"aaa"},
+			{"id":1, "coord":[3,4],"text":"bbb"},
+			{"id":2, "coord":[5,6],"text":"ccc"}
 		],
-	"line1": 
+	"edges":
 		[
-			{"node0":[1,2],"text":"aaa"},
-			{"node1":[3,4],"text":"bbb"}
+			{"id":0, 
+			 "endpt1":0, "endpt2":1, 
+			 "direction":1, "text":"zzz"
+			},
+			{"id":0, 
+			 "endpt1":1, "endpt2":2, 
+			 "direction":3, "text":"yyy"
+			}
 		]
 };
-
-console.log(example.edges[1].node1);
-
-//i want to go from 'w'=>'linearray[1][1]'
-
-/*
-edges
-	edgenum
-	edgetext
-	endpointnode1name
-	endpointnode2name
-	direction
-
-nodes
-	nodenum
-	nodetext
-	coord_x
-	coord_y
-*/
+console.log(data);
